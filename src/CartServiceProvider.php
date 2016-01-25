8f949470e6b25958e6b45ad9069eed66f42d2c26
Andrew <browner12@gmail.com>
2016-01-24 22:06:04 -0600

    new 'Cart' config file

--- src/CartServiceProvider.php
+++ src/CartServiceProvider.php
@@ -12,4 +12,14 @@ class CartServiceProvider extends ServiceProvider
         $this->app->bind('browner12\cart\CartInterface', 'browner12\cart\Cart');
     }
 
+    /**
+     * boot
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/cart.php' => config_path('cart.php'),
+        ]);
+    }
+
 }
