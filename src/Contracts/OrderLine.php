33c13f00f3be1062bfaf1a9c9001d3a1159987c2
Andrew <browner12@gmail.com>
2016-01-24 22:06:19 -0600

    add some methods to my interfaces

--- src/Contracts/OrderLine.php
+++ src/Contracts/OrderLine.php
@@ -2,5 +2,31 @@
 
 interface OrderLine
 {
+    /**
+     * get the product
+     *
+     * @return \browner12\cart\Contracts\Product
+     */
+    public function getProduct();
 
+    /**
+     * get quantity of the order line
+     *
+     * @return int
+     */
+    public function getQuantity();
+
+    /**
+     * get value of the order line
+     *
+     * @return int
+     */
+    public function getValue();
+
+    /**
+     * get weight of the order line
+     *
+     * @return float
+     */
+    public function getWeight();
 }
--- src/Contracts/Product.php
+++ src/Contracts/Product.php
@@ -2,5 +2,17 @@
 
 interface Product
 {
+    /**
+     * get value of the product
+     *
+     * @return int
+     */
+    public function getValue();
 
+    /**
+     * get weight of the product
+     *
+     * @return float
+     */
+    public function getWeight();
 }
